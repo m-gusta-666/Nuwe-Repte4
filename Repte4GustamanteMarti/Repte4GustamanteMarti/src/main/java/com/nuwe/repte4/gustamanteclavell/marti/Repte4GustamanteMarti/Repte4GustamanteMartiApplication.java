a9ff7394d69861717ca25f1c6471229f2c57c34c
m-gusta-666 <martigustamante@gmail.com>
2022-07-20 12:30:37 +0200

    Project creation

---
+++ Repte4GustamanteMarti/Repte4GustamanteMarti/src/main/java/com/nuwe/repte4/gustamanteclavell/marti/Repte4GustamanteMarti/Repte4GustamanteMartiApplication.java
@@ -0,0 +1,13 @@
+package com.nuwe.repte4.gustamanteclavell.marti.Repte4GustamanteMarti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Repte4GustamanteMartiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Repte4GustamanteMartiApplication.class, args);
+	}
+
+}
---
+++ Repte4GustamanteMarti/Repte4GustamanteMarti/src/test/java/com/nuwe/repte4/gustamanteclavell/marti/Repte4GustamanteMarti/Repte4GustamanteMartiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuwe.repte4.gustamanteclavell.marti.Repte4GustamanteMarti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Repte4GustamanteMartiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
