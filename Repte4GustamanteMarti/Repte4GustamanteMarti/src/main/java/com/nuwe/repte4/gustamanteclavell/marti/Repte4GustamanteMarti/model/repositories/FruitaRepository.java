1e047b0118bbcabf308e0ee5e2943936c510583e
m-gusta-666 <martigustamante@gmail.com>
2022-07-20 12:42:49 +0200

    Repository creation

---
+++ Repte4GustamanteMarti/Repte4GustamanteMarti/src/main/java/com/nuwe/repte4/gustamanteclavell/marti/Repte4GustamanteMarti/model/repositories/FruitaRepository.java
@@ -0,0 +1,9 @@
+package com.nuwe.repte4.gustamanteclavell.marti.Repte4GustamanteMarti.model.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nuwe.repte4.gustamanteclavell.marti.Repte4GustamanteMarti.model.domain.Fruita;
+
+public interface FruitaRepository extends JpaRepository<Fruita,Integer>{
+
+}
---
+++ Repte4GustamanteMarti/Repte4GustamanteMarti/src/main/java/com/nuwe/repte4/gustamanteclavell/marti/Repte4GustamanteMarti/model/repositories/VerduraRepository.java
@@ -0,0 +1,9 @@
+package com.nuwe.repte4.gustamanteclavell.marti.Repte4GustamanteMarti.model.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nuwe.repte4.gustamanteclavell.marti.Repte4GustamanteMarti.model.domain.Verdura;
+
+public interface VerduraRepository extends JpaRepository<Verdura,Integer>{
+
+}
